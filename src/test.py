ad9bb608c6f80de61e7efd9bec6fa7fc0743f9c5
Sai Teja <amarvajsaiteja@gmail.com>
2024-06-10 11:18:18 +0530

    Updated testcases for division operation in test.py

--- src/test.py
+++ src/test.py
@@ -1,8 +1,12 @@
 # app.py
 #This is a test change
-def add(a, b):
-    return a + b
+def divide(a, b):
+    assert b != 0, "Zero Division Error"
+    return a/b
 
-def test_add():
-    assert add(1, 2) == 3
-    assert add(1, -1) == 0
+def test_division():
+    divide(2, 1) == 2
+    print(f'** 1st test passed **\n')
+    divide(1, 0) == 1
+    print(f'** 2nd test passed **')
+test_division()
